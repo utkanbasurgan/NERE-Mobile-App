f92c7d94ee24d7c1b3496198e20955f320939cdb
Utkan Başurgan <140505686+utkanbasurgan@users.noreply.github.com>
2025-03-12 15:20:23 +0300

    Add files via upload

---
+++ CoinarthApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinarthApp.swift
+//  Coinarth
+//
+//  Created by Utkan Başurgan on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinarthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
